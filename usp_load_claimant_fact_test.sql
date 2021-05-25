e21fe9fe314b150f1211a93a5983093601bc81fb
SunnyW2019 <46783445+SunnyW2019@users.noreply.github.com>
2021-05-25 13:08:13 -0400

    add a test file

---
+++ usp_load_claimant_fact_test.sql
@@ -0,0 +1,14 @@
+USE [ENERGI_RDM01]
+GO
+/****** Object:  StoredProcedure [RDM].[usp_Load_Claimant_Fact]    Script Date: 5/24/2021 4:08:58 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+ALTER procedure [RDM].[usp_Load_Claimant_Fact]
+as 
+truncate table [RDM].[Claimant_Fact]
+insert into [RDM].[Claimant_Fact]
+select  name, id, address, employer from stage.test_profile;
